5ddb3986ca474017e491446d37fa4483a1e0ef38
João Henrique Ferreira <157930541+J-Ferre1ra@users.noreply.github.com>
2026-07-05 20:55:24 -0300

    Update gestaoClinica.js
    Adicionado a atualização de Status do paciente.
    Adicionado cancelamento de atendimentos.
    Adicionado a geração de relatório, tanto do total de pacientes quanto dos pacientes aguardando a triagem.

--- gestaoClinica.js
+++ gestaoClinica.js
@@ -27,3 +27,30 @@ cadastrarPaciente("", 30)
 cadastrarPaciente('Lucas', 20)
 
 console.log("Banco de dados atual:", pacientes);
+
+
+function atualizarStatus(id, novoStatus) {
+
+    pacientes = pacientes.map(paciente =>{
+        if (paciente.id === id) {
+            return {...paciente, status: novoStatus}
+        }
+        return paciente
+    })
+}
+
+function cancelarAtendimento(id) {
+    const fichaAtendimentos = pacientes.filter(paciente =>paciente.id !== id)
+
+    pacientes = fichaAtendimentos
+}
+
+function gerarRelatorio() {
+    const totalGeral = pacientes.length
+
+    const aguardando = pacientes.filter(paciente => paciente.status === "Aguardando Triagem")
+
+    console.log(`Total: ${totalGeral}`);
+    console.log(`Aguardando: ${aguardando.length}`);
+    
+}
